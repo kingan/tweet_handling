66993006a1a6f44451cc2ef1a9cb9847ce62e00a
Andrew King <kingan@tcd.ie>
2015-11-09 14:23:24 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ chmod a+x ./src/tweets_cleaned.py
 
 #Execute python script tweets_cleaned.py with appropriate parameters
 python ./src/tweets_cleaned.py ./tweet_input/tweets_cut.txt ./tweet_output/ft1.txt
+
+
