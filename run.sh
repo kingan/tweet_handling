8529f39bde7dc8e10211ab40150e8f7142f4f112
Andrew King <kingan@tcd.ie>
2015-11-09 14:19:43 +0000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#Ensure proper permissions are set
+chmod a+x ./src/tweets_cleaned.py
+
+#Execute python script tweets_cleaned.py with appropriate parameters
+python ./src/tweets_cleaned.py ./tweet_input/tweets_cut.txt ./tweet_output/ft1.txt
